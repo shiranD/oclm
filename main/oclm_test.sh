354169b5337eadc901903ddea90359049b82c4fa
Shiran Dudy <dudy@ohsu.edu>
2018-07-31 13:02:07 -0700

    shell test

--- main/oclm_test.sh
+++ main/oclm_test.sh
@@ -19,7 +19,6 @@ echo ------------------
 for fold in $(seq 1 $end)
 do
   machines=machines_${fold}
-  machines=/Users/dudy/CSLU/bci/5th_year/letters/evaluate/new_oclm/machines
   # rm logger if found
   export LD_LIBRARY_PATH=/usr/local/lib/fst
   python test.py --test ${datapath}/${fold}${tst} --eeg eeg_data/EEGEvidence.txt-high --nbest $nbest --machines ${machines} --output ${out}/${fold}_${nbest} --fold ${fold}
