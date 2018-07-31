718be87669fe4d4b191bc1885b7cf953bd6a13bb
Shiran Dudy <dudy@ohsu.edu>
2018-07-30 21:20:09 -0700

    client server

---
+++ main/server_mode/client.py
@@ -0,0 +1,41 @@
+import requests, json, server 
+from eeg_data.eeg_utils import simulate_eeg
+
+nbest=3
+#localhost = 'http://localhost:5000' # choose a port
+path2eeg='eeg_data/EEGEvidence.txt-high'
+simulator = simulate_eeg(path2eeg)
+
+# init
+r = requests.post(localhost+'/init', json={'nbest': nbest})
+print r.status_code
+
+# provide priors for a single likelihood evidence
+# provide words for the same evidence
+history='th'
+# build evidence history
+evidence = []
+for target in history:
+  evidence.extend(simulator.simulate(target))
+print evidence
+print "Charachter and Word distributions"
+r = requests.post(localhost+'/state_update', json={'evidence': evidence, 'return_mode': 'word'})
+print r.status_code
+e = r.json()
+
+# print priors for a history of evidence
+print e['letter']
+print e['word']
+
+# reset
+r = requests.post(localhost+'/reset')
+print r.status_code
+
+# feed new evidence and ask just for letter distribution
+print "Character distribution"
+target='y'
+evidence = simulator.simulate(target)
+r = requests.post(localhost+'/state_update', json={'evidence': evidence, 'return_mode': 'letter'})
+print r.status_code
+e = r.json()
+print "status code of 200 implies a good connection"
---
+++ main/server_mode/server.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import oclm
+app = Flask(__name__)
+
+print "READING LANGUAGE MODEL.."
+path=TBD
+lm = oclm.server(path)
+
+@app.route('/state_update', methods=['POST'])
+def lang_model():
+    j = request.get_json()
+    eegs = j['evidence']
+    return_mode = j['return_mode']
+    out = lm.state_update(eegs, return_mode)
+    if return_mode == 'letter':
+        return jsonify(letter=out)
+    else:
+        return jsonify(letter=out[0], word=out[1])
+
+@app.route('/init', methods=['POST'])
+def init():
+    j = request.get_json()
+    nbest = j['nbest']
+    lm.init(nbest=nbest)
+    return "succeded initing"
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    lm.reset()
+    return "succeded reseting"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
