a64f5ff4358f8cb885c752f0984b2deedc2ccac8
Shiran Dudy <dudy@ohsu.edu>
2018-07-31 13:05:42 -0700

    clean run

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ set -e
 
 test_name=_test.txt
 train_name=_train.txt
-corpus=Subtlex.US.txt # corpus of sentences
+corpus=TBD # corpus of sentences
 # make the following folders
 mkdir -p data
 mkdir -p json_output
@@ -15,10 +15,10 @@ datapath=../data
 out=../json_output
 ch_sym=../ch_syms.txt
 folds=0
-nbest=1
+nbest=1 # 1-3 is optimal
 
-python3 split.py --train $train_name --test $test_name --fdata $corpus --odir $datapath/ --folds $folds
-main/./oclm_train.sh $ch_sym $train_name $datapath $out $folds
-main/./oclm_test.sh $nbest $test_name $datapath $out $folds
+#python3 split.py --train $train_name --test $test_name --fdata $corpus --odir $datapath/ --folds $folds
+#main/./oclm_train.sh $ch_sym $train_name $datapath $out $folds
+#main/./oclm_test.sh $nbest $test_name $datapath $out $folds
 # Demo should be actively copied from demo dir to main dir
-#main/./oclm_demo.sh $nbest $test_name $datapath
+main/./oclm_demo.sh $nbest $test_name $datapath
